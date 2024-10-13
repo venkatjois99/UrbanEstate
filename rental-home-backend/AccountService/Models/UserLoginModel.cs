415c55bda5a6a229ff67df0ce3c88a96e81d095c
VENKAT JOIS K P <venkat.jois@happiestminds.com>
2024-10-13 12:28:49 +0000

    added backend-account service model

---
+++ rental-home-backend/AccountService/Models/UserLoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AccountService.Models
+{
+    public class UserLoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ rental-home-backend/AccountService/Models/UserRegisterModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AccountService.Models
+{
+    public class UserRegisterModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+       
+        public bool isAdmin {  get; set; }
+        public bool isOwner { get; set; }
+
+        public bool isTenant{ get; set; }
+
+    }
+}
